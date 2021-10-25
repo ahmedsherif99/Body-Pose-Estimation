5cb3ea36f2a1f9e7f71e406db85c48235ac864b5
Ahmed Soliman <68401714+ahmedsherif99@users.noreply.github.com>
2021-10-25 18:51:18 +0200

    Add files via upload

---
+++ pose module -message.py
@@ -0,0 +1,145 @@
+import cv2
+import mediapipe as mp
+import time
+import math
+import smtplib
+from time import sleep
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+flagi = 1
+# Email Variables
+SMTP_SERVER = 'smtp.gmail.com'  # Email Server (don't change!)
+SMTP_PORT = 587  # Server Port (don't change!)
+GMAIL_USERNAME = 'homealonerobot@gmail.com'  # change this to match your gmail account
+GMAIL_PASSWORD = 'Homealonerobotteam14'  # change this to match your gmail password
+
+
+class Emailer:
+    def sendmail(self, recipient, subject, content, image):
+        # Create Headers
+        emailData = MIMEMultipart()
+        emailData['Subject'] = subject
+        emailData['To'] = recipient
+        emailData['From'] = GMAIL_USERNAME
+        #Attach our text data
+        emailData.attach(MIMEText(content))
+        #Create our Image Data from the defined image
+        imageData = MIMEImage(open(image, 'rb').read(), 'jpg')
+        imageData.add_header('Content-Disposition', 'attachment; filename="image.jpg"')
+        emailData.attach(imageData)
+
+        # Connect to Gmail Server
+        session = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+        session.ehlo()
+        session.starttls()
+        session.ehlo()
+
+        # Login to Gmail
+        session.login(GMAIL_USERNAME, GMAIL_PASSWORD)
+
+        # Send Email & Exit
+        session.sendmail(GMAIL_USERNAME, recipient, emailData.as_string())
+        session.quit
+
+
+sender = Emailer()
+
+sendTo = 'ahmedgs2009@gmail.com'
+emailSubject = "Alert"
+emailContent = "There is an Intruder in the house"
+sendimage = cv2.imread('test.jpg',1)
+sleep(10)
+
+class poseDetector():
+
+    def __init__(self, mode=False, upBody=False, smooth=True,
+                 detectionCon=0.5, trackCon=0.5):
+
+        self.mode = mode
+        self.upBody = upBody
+        self.smooth = smooth
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpDraw = mp.solutions.drawing_utils
+        self.mpPose = mp.solutions.pose
+        self.pose = self.mpPose.Pose(self.mode, self.upBody, self.smooth,
+                                     self.detectionCon, self.trackCon)
+
+    def findPose(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.pose.process(imgRGB)
+        if self.results.pose_landmarks:
+            if draw:
+                self.mpDraw.draw_landmarks(img, self.results.pose_landmarks,
+                                           self.mpPose.POSE_CONNECTIONS)
+        return img
+
+    def findPosition(self, img, draw=True):
+        self.lmList = []
+        if self.results.pose_landmarks:
+            for id, lm in enumerate(self.results.pose_landmarks.landmark):
+                h, w, c = img.shape
+                # print(id, lm)
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                self.lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+        return self.lmList
+
+    def findAngle(self, img, p1, p2, p3, draw=True):
+
+        # Get the landmarks
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p2][1:]
+        x3, y3 = self.lmList[p3][1:]
+
+        # Calculate the Angle
+        angle = math.degrees(math.atan2(y3 - y2, x3 - x2) -
+                             math.atan2(y1 - y2, x1 - x2))
+        if angle < 0:
+            angle += 360
+
+        # print(angle)
+
+        # Draw
+        if draw:
+            cv2.line(img, (x1, y1), (x2, y2), (255, 255, 255), 3)
+            cv2.line(img, (x3, y3), (x2, y2), (255, 255, 255), 3)
+            cv2.circle(img, (x1, y1), 10, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x1, y1), 15, (0, 0, 255), 2)
+            cv2.circle(img, (x2, y2), 10, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 15, (0, 0, 255), 2)
+            cv2.circle(img, (x3, y3), 10, (0, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x3, y3), 15, (0, 0, 255), 2)
+            cv2.putText(img, str(int(angle)), (x2 - 50, y2 + 50),
+                        cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+        return angle
+
+def main():
+    cap = cv2.VideoCapture(0)
+    pTime = 0
+    detector = poseDetector()
+    while True:
+        success, img = cap.read()
+        img = detector.findPose(img)
+        lmList = detector.findPosition(img, draw=False)
+        if len(lmList) != 0:
+            print(lmList[14])
+            cv2.circle(img, (lmList[14][1], lmList[14][2]), 15, (0, 0, 255), cv2.FILLED)
+            sender.sendmail(sendTo, emailSubject, emailContent, sendimage)
+            ser.write(str(1).encode('utf-8'))
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+
+        cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3,
+                    (255, 0, 0), 3)
+
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+
+
+if __name__ == "__main__":
+    main()
